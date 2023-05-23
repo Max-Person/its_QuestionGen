c92f0969c5dd38247cd05e6e1d617f077aa003ec
Max-Person <max_person@mail.ru>
2023-05-23 18:21:24 +0300

    [feat] возможность системно-независимого создания модели через URL

--- src/its/questions/gen/formulations/LocalizedDomainModel.kt
+++ src/its/questions/gen/formulations/LocalizedDomainModel.kt
@@ -6,21 +6,24 @@ import com.google.gson.stream.JsonReader
 import its.model.DomainModel
 import its.model.dictionaries.*
 import its.model.models.*
-import java.io.FileReader
+import java.io.File
+import java.net.URL
 
-class LocalizedDomainModel(directory: String) : DomainModel<ClassModel, DecisionTreeVarModel, EnumModel, PropertyModel, RelationshipModel>(
+class LocalizedDomainModel(directoryURL: URL) : DomainModel<ClassModel, DecisionTreeVarModel, EnumModel, PropertyModel, RelationshipModel>(
     ClassesDictionary(),
     DecisionTreeVarsDictionary(),
     EnumsDictionary(),
     PropertiesDictionary(),
     RelationshipsDictionary(),
-    directory
+    directoryURL
     )
 {
+    constructor(directory: String) : this(File(directory).toURI().toURL())
+
     val localizations : Map<String, DomainLocalization>
 
     init {
-        val reader = JsonReader(FileReader(directory + "localization.json"))
+        val reader = JsonReader((directoryURL + "localization.json").openStream().bufferedReader())
         val obj = Gson().fromJson<JsonObject>(reader, JsonObject::class.java)
         localizations = obj.entrySet().map {
             if(Localization.localizations.containsKey(it.key)){
