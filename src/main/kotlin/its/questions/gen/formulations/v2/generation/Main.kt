df8a4bc45c68cf47991efa946801a127a0bf30a3
Max-Person <max_person@mail.ru>
2025-05-21 21:59:05 +0300

    [change] тестовый мейн и повышение версии

--- src/main/kotlin/its/questions/gen/formulations/v2/generation/Main.kt
+++ src/main/kotlin/its/questions/gen/formulations/v2/generation/Main.kt
@@ -7,12 +7,63 @@ import its.reasoner.LearningSituation
 import java.io.StringReader
 
 fun main() {
-    val loqiStr = """
+
+    val domainModel = DomainLoqiBuilder.buildDomain(StringReader(loqiStr))
+    domainModel.validateAndThrow()
+
+    val exprs = listOf(
+
+        "obj:murzik.age",
+        "obj:murzik.age.compare(2)",
+        "obj:murzik.age == 2",
+        "obj:murzik.age != 2",
+        "obj:murzik.age > 2",
+        "obj:murzik.age < 2",
+        "obj:murzik.age >= 2",
+
+        "obj:murzik.age.compare(obj:basya.age)",
+        "obj:murzik.age == obj:basya.age",
+        "obj:murzik.age != obj:basya.age",
+        "obj:murzik.age > obj:basya.age",
+        "obj:murzik.age < obj:basya.age",
+        "obj:murzik.age >= obj:basya.age",
+
+        "obj:murzik.isCool",
+        "obj:murzik.isCool == true",
+        "obj:murzik.isCool == obj:basya.isCool",
+
+        "obj:alice=>hasPet(obj:murzik)",
+
+        "obj:murzik.class()",
+        "obj:murzik is Pet",
+    ).associateWith { OperatorLoqiBuilder.buildExp(it) }
+
+    exprs.forEach { (string, expr) ->
+        println(
+            string + "\t\t" + QuestionGeneratorFabric(
+                LearningSituation(domainModel, HashMap()),
+                LocalizationRU
+            ).generateQuestion(expr)
+        )
+    }
+
+}
+
+
+val loqiStr = """
         //класс "Питомец"  
         class Pet {  
         	//"Возраст" - Объектное свойство данного класса  
-        	obj prop age: int [RU.name = "красота";];  
-        }  
+        	obj prop age: int [
+                RU.localizedName = "возраст";
+            ];  
+            
+            obj prop isCool: bool [
+                RU.localizedName = "крутой";
+            ];
+        }  [
+            RU.localizedName = "питомец";
+        ]
 
         //класс "Человек"  
         class Human {  
@@ -20,14 +71,28 @@ fun main() {
         	obj prop age: int;  
         	//"Имеет питомца" - отношение между объектами класса "Человек"  
         	// и объектами класса "Питомец"  
-        	rel hasPet(Pet) : {1 -> *} ;  
-        }  
+        	rel hasPet(Pet) : {1 -> *} [
+                RU.assertion = "${'$'}{${'$'}subj} имеет питомца ${'$'}{${'$'}obj1}";
+            ];  
+        }  [
+            RU.localizedName = "человек";
+        ]
 
         //Объект класса "Питомец", представляющий конкретного питомца "Мурзик"  
         obj murzik : Pet {  
         	//Утверждение о значении свойства "Возраст" данного объекта  
         	age = 2;  
-        }  [RU.localizedName = "мурзик";]
+            isCool = false;
+        }  [
+            RU.localizedName = "Мурзик";
+        ]
+        
+        obj basya : Pet {  
+        	age = 5;  
+            isCool = true;
+        }  [
+            RU.localizedName = "Бася";
+        ]
 
         //Объект класса "Человек", представляющий конкретного человека "Алиса"  
         obj alice : Human {  
@@ -36,19 +101,8 @@ fun main() {
         	//Утверждение о связи данного объекта 
         	//с другим объектом "Мурзик" по отношению "имеет питомца" 
         	hasPet(murzik);  
-        }  
+        }  [
+            RU.localizedName = "Алиса";
+        ]
         
     """.trimIndent()
-
-    val domainModel = DomainLoqiBuilder.buildDomain(StringReader(loqiStr))
-    domainModel.validateAndThrow()
-
-    val expr = OperatorLoqiBuilder.buildExp("""
-        obj:murzik.age == 2
-    """.trimIndent())
-
-    println(QuestionGeneratorFabric(
-        LearningSituation(domainModel, HashMap()),
-        LocalizationRU
-    ).generateQuestion(expr))
-}
