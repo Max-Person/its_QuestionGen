d6325dc1f8ae24a74baa650672cd58feeaed81a0
Yulia Gomazkova <gomazkova19@yandex.ru>
2025-05-25 20:36:01 +0300

    refactor: перенос генерации вопросов и ответов в контекст

--- src/main/kotlin/its/questions/gen/formulations/v2/generation/constant/CompareWithConstant.kt
+++ src/main/kotlin/its/questions/gen/formulations/v2/generation/constant/CompareWithConstant.kt
@@ -18,54 +18,10 @@ import its.reasoner.LearningSituation
 import its.reasoner.operators.OperatorReasoner
 
 abstract class CompareWithConstant(learningSituation: LearningSituation, localization: Localization) :
-    AbstractQuestionGeneration<CompareWithConstant.CompareWithConstantContext>(
+    AbstractQuestionGeneration(
         learningSituation,
         localization
     ) {
-    override fun generate(context: CompareWithConstantContext): String? {
-        val questionOrAssertion = getQuestionOrAssertion(context)
-        if (questionOrAssertion != null) {
-            return questionOrAssertion
-        }
-        val obj = context.objExpr.use(OperatorReasoner.defaultReasoner(learningSituation)) as Obj
-        return localization.COMPARE_A_PROPERTY_TO_A_CONSTANT(
-            context.propertyDef.getLocalizedName(localization.codePrefix),
-            obj.findIn(learningSituation.domainModel)!!
-                .getLocalizedName(localization.codePrefix),
-            context.valueConstant.value.toLocalizedString(learningSituation, localization.codePrefix)
-        )
-    }
-
-    protected fun getQuestionOrAssertion(context: CompareWithConstantContext): String? {
-        val question = context.propertyDef.metadata.getString(localization.codePrefix, "question")
-        val obj = context.objExpr.use(OperatorReasoner.defaultReasoner(learningSituation)) as Obj
-        val contextVars = mutableMapOf(
-            "obj" to obj,
-            "value" to context.valueConstant.value
-        )
-        context.paramsMap.forEach { (paramName, operator) ->
-            contextVars[paramName] = operator.use(OperatorReasoner.defaultReasoner(learningSituation))!!
-        }
-        if (question != null) {
-            return question.interpret(learningSituation, localization.codePrefix, contextVars)
-        }
-        val assertion = context.propertyDef.metadata.getString(localization.codePrefix, "assertion")
-        if (assertion != null) {
-            val interpreted = assertion.interpret(learningSituation, localization.codePrefix, contextVars)
-            return localization.IS_IT_TRUE_THAT(interpreted)
-        }
-        return null
-    }
-
-    override fun generateAnswer(context: CompareWithConstantContext, value: Any): String? {
-        var valueToPass = value
-        if ((context.operator == CompareWithComparisonOperator.ComparisonOperator.NotEqual ||
-                    context.operator == CompareWithComparisonOperator.ComparisonOperator.GreaterEqual ||
-                    context.operator == CompareWithComparisonOperator.ComparisonOperator.LessEqual) && value is Boolean) {
-            valueToPass = !value
-        }
-        return super.generateAnswer(context, valueToPass)
-    }
 
     override fun fits(operator: Operator): CompareWithConstantContext? {
         if (operator is CompareWithComparisonOperator
@@ -79,7 +35,7 @@ abstract class CompareWithConstant(learningSituation: LearningSituation, localiz
             val propertyDef = propertyValue.getPropertyDef(learningSituation.domainModel)
             val paramsMap = propertyValue.paramsValues.asMap(propertyDef.paramsDecl)
             if (typeFits(propertyDef.type)) {
-                return CompareWithConstantContext(valueLiteral, operator.operator, objExpr, propertyDef, paramsMap)
+                return createContext(valueLiteral, operator.operator, objExpr, propertyDef, paramsMap)
             }
         }
         if (operator is Compare
@@ -94,20 +50,73 @@ abstract class CompareWithConstant(learningSituation: LearningSituation, localiz
             val propertyDef = propertyValue.getPropertyDef(learningSituation.domainModel)
             val paramsMap = propertyValue.paramsValues.asMap(propertyDef.paramsDecl)
             if (typeFits(propertyDef.type)) {
-                return CompareWithConstantContext(valueLiteral, null, objExpr, propertyDef, paramsMap)
+                return createContext(valueLiteral, null, objExpr, propertyDef, paramsMap)
             }
         }
         return null
     }
 
+    protected open fun createContext(valueLiteral: ValueLiteral<*, *>,
+                                     operator: CompareWithComparisonOperator.ComparisonOperator?,
+                                     objExpr: Operator,
+                                     propertyDef: PropertyDef,
+                                     paramsMap: Map<String, Operator>) : CompareWithConstantContext  {
+        return CompareWithConstantContext(valueLiteral, operator, objExpr, propertyDef, paramsMap)
+    }
+
     protected abstract fun typeFits(type: Type<*>): Boolean
 
-    class CompareWithConstantContext(
+    open class CompareWithConstantContext(
         val valueConstant: ValueLiteral<*, *>, // справа от оператора сравнения
         val operator: CompareWithComparisonOperator.ComparisonOperator?, // оператор
         val objExpr: Operator, // переменная
         val propertyDef: PropertyDef, // для получения мета данных (локализации и тд)
         val paramsMap: Map<String, Operator>
-    ) : AbstractContext
+    ) : AbstractContext {
+        override fun generate(learningSituation: LearningSituation, localization: Localization): String? {
+            val questionOrAssertion = getQuestionOrAssertion(learningSituation, localization)
+            if (questionOrAssertion != null) {
+                return questionOrAssertion
+            }
+            val obj = objExpr.use(OperatorReasoner.defaultReasoner(learningSituation)) as Obj
+            return localization.COMPARE_A_PROPERTY_TO_A_CONSTANT(
+                propertyDef.getLocalizedName(localization.codePrefix),
+                obj.findIn(learningSituation.domainModel)!!
+                    .getLocalizedName(localization.codePrefix),
+                valueConstant.value.toLocalizedString(learningSituation, localization.codePrefix)
+            )
+        }
+
+        protected fun getQuestionOrAssertion(learningSituation: LearningSituation, localization: Localization): String? {
+            val question = propertyDef.metadata.getString(localization.codePrefix, "question")
+            val obj = objExpr.use(OperatorReasoner.defaultReasoner(learningSituation)) as Obj
+            val contextVars = mutableMapOf(
+                "obj" to obj,
+                "value" to valueConstant.value
+            )
+            paramsMap.forEach { (paramName, operator) ->
+                contextVars[paramName] = operator.use(OperatorReasoner.defaultReasoner(learningSituation))!!
+            }
+            if (question != null) {
+                return question.interpret(learningSituation, localization.codePrefix, contextVars)
+            }
+            val assertion = propertyDef.metadata.getString(localization.codePrefix, "assertion")
+            if (assertion != null) {
+                val interpreted = assertion.interpret(learningSituation, localization.codePrefix, contextVars)
+                return localization.IS_IT_TRUE_THAT(interpreted)
+            }
+            return null
+        }
+
+        override fun generateAnswer(learningSituation: LearningSituation, localization: Localization, value : Any): String? {
+            var valueToPass = value
+            if ((operator == CompareWithComparisonOperator.ComparisonOperator.NotEqual ||
+                        operator == CompareWithComparisonOperator.ComparisonOperator.GreaterEqual ||
+                        operator == CompareWithComparisonOperator.ComparisonOperator.LessEqual) && value is Boolean) {
+                valueToPass = !value
+            }
+            return super.generateAnswer(learningSituation, localization, valueToPass)
+        }
+    }
 
 }
